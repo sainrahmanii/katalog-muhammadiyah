16f17ea6650de3ea2d5779bbf7b4b2831c0e70eb
sainrahmanii <husainrahmani127@gmail.com>
2023-01-09 13:03:50 +0700

    database

---
+++ database/migrations/2023_01_09_055248_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('detail');
+            $table->string('image');
+            $table->bigInteger('seller_id')->unsigned();
+            $table->boolean('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
